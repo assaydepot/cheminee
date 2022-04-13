2c65427d819e92803b60dccb3ed23a159b7ff4d2
Maria Dubyaga <kraftaa@gmail.com>
2022-04-13 10:19:00 -0400

    added some tests to poke around

--- src/actions/index_pubchem_sdf.rs
+++ src/actions/index_pubchem_sdf.rs
@@ -4,6 +4,9 @@ use ndarray::arr2;
 // use petal_decomposition::{Pca, PcaBuilder};
 use smartcore::linalg::naive::dense_matrix::*;
 use smartcore::decomposition::pca::*;
+use linfa::traits::{Fit, Predict};
+use linfa_reduction::Pca as linfa_pca;
+
 pub const NAME: &'static str = "index-pubchem-sdf";
 
 pub fn command() -> Command<'static> {
@@ -43,10 +46,29 @@ pub fn action(matches: &ArgMatches) -> eyre::Result<()> {
     let mut index_writer = index.writer_with_num_threads(1, 50 * 1024 * 1024)?;
 
         for mol_block in mol_iter {
-            let mol = match Molecule::new(&mol_block, "") {
+            let mut mol = match Molecule::new(&mol_block, "") {
                 Some(m) => m,
                 None => continue,
             };
+            println!("mol {:?}", mol);
+            // println!("{:?}  get_qmol", mol.get_qmol(""));
+            mol.cleanup("");
+            println!("mol  cleanup {:?}", mol);
+            // println!("{:?}  get_qmol", mol.get_qmol(""));
+            // println!("mol  reionize {:?}", mol.reionize(""));
+            // println!("mol  canonical_tautomer {:?}", mol.canonical_tautomer(""));
+            println!("{:?} parent fragment", mol.fragment_parent(""));
+
+            println!("{:?}  get_rdkit_fp", mol.get_rdkit_fp(""));
+            println!("{:?}  get_pattern_fp", mol.get_pattern_fp(""));
+            println!("{:?}  get_pattern_fp_as_bytes", mol.get_pattern_fp_as_bytes(""));
+            // parent_clean_mol.reionize("");
+            println!("reionize {:?}", mol.reionize(""));
+            println!("neutralize {:?}", mol.neutralize(""));
+            println!("canonical  {:?}", mol.canonical_tautomer(""));
+
+            // println!("canonical  {:?}", mol.canonical_tautomer(""));
+            // println!("mol  fragment_parent {:?}", mol.fragment_parent(""));
 
             let smile = schema.get_field("smile").unwrap();
             let descriptors = schema.get_field("descriptors").unwrap();
@@ -83,10 +105,57 @@ pub fn action(matches: &ArgMatches) -> eyre::Result<()> {
             println!("{:?} coord", coord);
             // let clog = json.get("CrippenClogP").unwrap().clone();
             let mut clog = json.as_object().unwrap()["CrippenClogP"].clone();
+            // &Vec<Vec<T>>
+            let mut outer_vec: Vec<Vec<f64>> = vec![vec![]];
+            let mut inner_vec:Vec<_> = vec![0.0];
             println!("{:?} clog ", clog);
             for (name, obj) in json.as_object().unwrap().iter() {
                 println!("{} is {:?}", name, obj);
+                inner_vec.push(json!(obj).as_f64().unwrap());
+                println!("inner vec {:?}", inner_vec)
             }
+            outer_vec.push(inner_vec);
+            println!("{:?} outer vec", outer_vec);
+            let matrix = DenseMatrix::from_2d_vec(&outer_vec);
+            println!("matrix ta dam {:?}", matrix);
+            // let dataset = linfa_datasets::iris();
+
+            // let embedding = linfa_pca::params(1)
+            //     .fit(&outer_vec).unwrap();
+            // let dataset = embedding.predict(dataset);
+            // ///
+            use smartcore::linalg::naive::dense_matrix::*;
+            use smartcore::decomposition::pca::*;
+
+// Iris data
+            let iris = DenseMatrix::from_2d_array(&[
+                &[5.1, 3.5, 1.4, 0.2],
+                &[4.9, 3.0, 1.4, 0.2],
+                &[4.7, 3.2, 1.3, 0.2],
+                &[4.6, 3.1, 1.5, 0.2],
+                &[5.0, 3.6, 1.4, 0.2],
+                &[5.4, 3.9, 1.7, 0.4],
+                &[4.6, 3.4, 1.4, 0.3],
+                &[5.0, 3.4, 1.5, 0.2],
+                &[4.4, 2.9, 1.4, 0.2],
+                &[4.9, 3.1, 1.5, 0.1],
+                &[7.0, 3.2, 4.7, 1.4],
+                &[6.4, 3.2, 4.5, 1.5],
+                &[6.9, 3.1, 4.9, 1.5],
+                &[5.5, 2.3, 4.0, 1.3],
+                &[6.5, 2.8, 4.6, 1.5],
+                &[5.7, 2.8, 4.5, 1.3],
+                &[6.3, 3.3, 4.7, 1.6],
+                &[4.9, 2.4, 3.3, 1.0],
+                &[6.6, 2.9, 4.6, 1.3],
+                &[5.2, 2.7, 3.9, 1.4],
+            ]);
+
+            let pca = PCA::fit(&iris, PCAParameters::default().with_n_components(1)).unwrap(); // Reduce number of features to 2
+            println!("{:?} pca", pca);
+            let iris_reduced = pca.transform(&iris).unwrap();
+            println!("iris {:?}", iris_reduced);
+            ///
 
             let crip = json.as_object().unwrap()["CrippenMR"].clone();
             println!("{:?} crip ", crip);
@@ -151,4 +220,68 @@ pub fn action(matches: &ArgMatches) -> eyre::Result<()> {
     index_writer.commit()?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bad_mol1() {
+        let molblock = "THIOL_12\n     RDKit          3D\n\n 25 25  0  0  0  0  0  0  0  0999 V2000\n   -2.2510   -2.6650   -2.0550 S   0  0  0  0  0  0  0  0  0  0  0  0\n   -3.3040   -2.7120   -2.1100 H   0  0  0  0  0  0  0  0  0  0  0  0\n   -1.7910   -1.5140   -0.7240 C   0  0  0  0  0  0  0  0  0  0  0  0\n   -2.1270   -2.0270    0.1920 H   0  0  0  0  0  0  0  0  0  0  0  0\n   -2.4730   -0.6640   -0.8710 H   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.2780   -0.7500   -0.3280 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.2420   -1.8480   -0.5140 O   0  0  0  0  0  0  0  0  0  0  0  0\n    0.4860    0.3560   -0.2740 N   0  0  0  0  0  0  0  0  0  0  0  0\n    0.0540    1.2670   -0.1190 H   0  0  0  0  0  0  0  0  0  0  0  0\n    1.9050    0.2450   -0.7390 C   0  0  1  0  0  0  0  0  0  0  0  0\n    1.9190   -0.1360   -1.7740 H   0  0  0  0  0  0  0  0  0  0  0  0\n    2.4830    1.6820   -0.6980 C   0  0  0  0  0  0  0  0  0  0  0  0\n    2.4150    2.0880    0.3240 H   0  0  0  0  0  0  0  0  0  0  0  0\n    3.5420    1.6740   -0.9990 H   0  0  0  0  0  0  0  0  0  0  0  0\n    1.7270    2.5420   -1.6810 C   0  0  0  0  0  0  0  0  0  0  0  0\n    1.7070    2.3180   -2.9770 N   0  0  0  0  0  0  0  0  0  0  0  0\n    2.2060    1.5550   -3.4590 H   0  0  0  0  0  0  0  0  0  0  0  0\n    0.9600    3.1990   -3.5600 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.7540    3.2500   -4.6280 H   0  0  0  0  0  0  0  0  0  0  0  0\n    0.5040    3.9880   -2.6970 N   0  0  0  0  0  0  0  0  0  0  0  0\n    0.9760    3.6220   -1.3870 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.7740    4.0890   -0.4240 H   0  0  0  0  0  0  0  0  0  0  0  0\n    2.7730   -0.7080    0.0810 C   0  0  0  0  0  0  0  0  0  0  0  0\n    3.9440   -0.8660   -0.2210 O   0  0  0  0  0  0  0  0  0  0  0  0\n    2.2450   -1.3190    1.1390 O   0  0  0  0  0  0  0  0  0  0  0  0\n  1  2  1  0\n  1  3  1  0\n  3  4  1  0\n  3  5  1  0\n  3  6  1  0\n  6  7  2  0\n  6  8  1  0\n  8  9  1  0\n  8 10  1  0\n 10 11  1  6\n 10 12  1  0\n 10 23  1  0\n 12 13  1  0\n 12 14  1  0\n 12 15  1  0\n 15 16  1  0\n 15 21  2  0\n 16 17  1  0\n 16 18  1  0\n 18 19  1  0\n 18 20  2  0\n 20 21  1  0\n 21 22  1  0\n 23 24  2  0\n 23 25  1  0\nM  CHG  1  25  -1\nM  END\n";
+        let mut pkl_mol = Molecule::new(molblock, "").unwrap();
+        println!("1: {:?}", pkl_mol);
+        pkl_mol.cleanup(""); // this is needed to avoid exception...
+        //pkl_mol2.remove_all_hs();
+        println!("2: {:?}", pkl_mol);
+        println!("3.0: {:?}", pkl_mol.get_smiles(""));
+        pkl_mol.canonical_tautomer("");
+        println!("3: {:?}", pkl_mol.get_smiles(""));
+        // println!("4: {:?}", pkl_mol.canonical_tautomer(""));
+        assert_eq!(pkl_mol.get_smiles(""), "O=C(CS)NC(Cc1c[nH]cn1)C(=O)[O-]");
+    }
+
+    #[test]
+    fn find_substructure() {
+        let mol = Molecule::new("Cl[C@H](F)C[C@H](F)Cl", "").unwrap();
+        let query_mol = Molecule::get_qmol("Cl[C@@H](F)C", "").unwrap();
+        let res = mol.get_substruct_match(&query_mol, "");
+        assert_eq!(res, "{\"atoms\":[0,1,2,3],\"bonds\":[0,1,2]}");
+        let res = mol.get_substruct_matches(&query_mol, "");
+        assert_eq!(
+            res,
+            "[{\"atoms\":[0,1,2,3],\"bonds\":[0,1,2]},{\"atoms\":[6,4,5,3],\"bonds\":[5,4,3]}]"
+        );
+    }
+
+    #[test]
+    fn normalize() {
+        let orig_smiles = "CN=N#N";
+        let mut pkl_mol = Molecule::new(orig_smiles, "").unwrap();
+        pkl_mol.normalize("");
+        let smiles = pkl_mol.get_smiles("");
+        assert_eq!(smiles, "CN=[N+]=[N-]");
+    }
+
+    #[test]
+    fn test_fragment_parent() {
+        let orig_smiles = "CC";
+        let mut pkl_mol = Molecule::new(orig_smiles, "").unwrap();
+        let result = pkl_mol.fragment_parent("");
+        println!("{:?} fragment parent", result);
+        panic!("{} => {:?}", result, pkl_mol);
+    }
+
+    #[test]
+    fn smiles_from_smiles_via_pkl() {
+        let orig_smiles = "OCCC#CO";
+        // let orig_smiles = "[H]C([H])([H])Sc1nc(N(C([H])([H])C([H])([H])C([H])([H])C([H])([H])[H])C([H])([H])C([H])([H])C([H])([H])C([H])([H])[H])c2sc3nc(C([H])([H])C([H])([H])C([H])([H])C([H])([H])[H])c4c(c3c2n1)C([H])([H])C([H])([H])C4([H])[H]";
+        let pkl_mol = Molecule::new(orig_smiles, "").unwrap();
+        println!(
+            "SMILES: {} Canonical SMILES: {}",
+            orig_smiles,
+            pkl_mol.get_smiles("")
+        );
+        assert_eq!(pkl_mol.get_smiles(""), "OC#CCCO");
+    }
 }
